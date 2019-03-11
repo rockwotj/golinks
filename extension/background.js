063299ab13f1d6fd6bf6e4d11e42904212c88d98
Tyler Rockwood <rockwotj@gmail.com>
2019-03-11 00:54:22 -0500

    Initial commit

---
+++ extension/background.js
@@ -0,0 +1,26 @@
+const PROJECT = "<PROJECT>";
+const BASE = "https://.appspot.com/";
+const GO = BASE + "visit?link=";
+const LIST = BASE + "list";
+
+chrome.webRequest.onBeforeRequest.addListener(function(details) {
+    let url = new URL(details.url);
+    // TODO: Consider forwarding query parameters too?
+    if (url.hostname == "go") {
+      return {redirectUrl: GO + encodeURIComponent(url.pathname.slice(1))};
+    }
+    return {};
+  },
+  {
+    urls: [
+      "<all_urls>",
+    ],
+    types: ["main_frame"]
+  },
+  ["blocking"]
+);
+
+chrome.browserAction.onClicked.addListener(function(tab) { 
+  chrome.tabs.create({ url: LIST });
+});
+
