9f4fff7ffdebc7bbedd7c1c27b431ed43f1d352a
Tyler Rockwood <tyler@shortwave.com>
2023-04-05 11:03:26 -0500

    Manifest v3

--- extension/background.js
+++ extension/background.js
@@ -1,26 +1,6 @@
-const PROJECT = "<PROJECT>";
-const BASE = "https://" + PROJECT + ".appspot.com/";
-const GO = BASE + "visit?link=";
-const LIST = BASE + "list";
-
-chrome.webRequest.onBeforeRequest.addListener(function(details) {
-    let url = new URL(details.url);
-    // TODO: Consider forwarding query parameters too?
-    if (url.hostname == "go") {
-      return {redirectUrl: GO + encodeURIComponent(url.pathname.slice(1))};
-    }
-    return {};
-  },
-  {
-    urls: [
-      "<all_urls>",
-    ],
-    types: ["main_frame"]
-  },
-  ["blocking"]
-);
-
-chrome.browserAction.onClicked.addListener(function(tab) { 
-  chrome.tabs.create({ url: LIST });
+chrome.action.onClicked.addListener((_) => { 
+  chrome.tabs.create({ url: "https://example.com/list" });
 });
+
+export {}
 
